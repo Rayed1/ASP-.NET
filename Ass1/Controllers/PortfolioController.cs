8cb681348be5ca5133150caa26e84fcfbc745255
Rayed <eshmamrayed99@gmail.com>
2023-09-22 19:36:45 +0600

    Add project files.

---
+++ Ass1/Controllers/PortfolioController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ass1.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        // GET: Portfolio
+       
+        public ActionResult Home()
+        {
+            return View();
+        }
+        public ActionResult Index() 
+        {
+            return View();
+        }
+        public ActionResult Personal() 
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Reference()
+        {
+            return View();
+        }
+        
+    }
+}
